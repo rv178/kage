8e271a2fe49f7dcfdc196ac5d2b55bde82750f69
rv178 <idliyout@gmail.com>
2022-09-22 20:16:09 +0530

    [feat] method to rotate bitboard 180 degrees

--- engine/src/bitboard.rs
+++ engine/src/bitboard.rs
@@ -57,70 +57,50 @@ pub fn convert(game_status: &mut GameStatus) {
     positions.wp.print();
 }
 
-impl BitBoard {
-    pub fn gen(pieces: &mut [Option<Piece>; 64], compare: char) -> Self {
-        let mut bin_str = String::new();
-        // reverse pieces array
-        pieces.reverse();
+// bit manipulation inside bitboard
+trait BitM {
+    fn set_bit(&mut self, square: Square);
+    fn toggle_bit(&mut self, square: Square);
+}
 
-        for piece in pieces {
-            if let Some(piece) = piece {
-                if !(piece.symbol == compare) {
-                    bin_str.push('0');
-                } else {
-                    bin_str.push('1');
-                }
-            } else {
-                bin_str.push('0');
-            }
-        }
+// rotation methods
+trait Rotation {
+    fn rot_180(&mut self);
+}
 
-        // convert binary string to decimal (u64)
-        Self(u64::from_str_radix(&bin_str, 2).unwrap())
-    }
-    // check if bitboard is empty (u64 = 0)
-    pub fn is_empty(&self) -> bool {
-        self.0 == 0
-    }
-    // check if equal
-    pub fn eq(self, other: Self) -> bool {
-        self.0 == other.0
-    }
-    // init empty bitboard
-    pub fn empty() -> Self {
-        Self(0)
-    }
-    pub fn from_sq(square: Square) -> Self {
-        Self(1 << square as u8)
-    }
-    // print bitboard
-    pub fn print(&self) {
-        println!("Value: {}", &self.0);
-        let mut x = 8;
-        for rank in 0..8 {
-            print!("{}  ", x);
-            x -= 1;
-            for file in 0..8 {
-                let square = rank * 8 + file;
-                if self.0 & (1 << square) >= 1 {
-                    print!("1 ");
-                } else {
-                    print!("0 ");
-                }
-            }
-            println!();
-        }
-        println!();
-        println!("   a b c d e f g h");
-        println!();
-    }
+/*
+// flip the bitboard
+trait Flip {
+    fn flip_v(&mut self);
+    fn flip_h(&mut self);
+}
+*/
+
+impl BitM for BitBoard {
     // set bit at given square (0 -> 1)
-    pub fn set_bit(&mut self, square: Square) {
+    fn set_bit(&mut self, square: Square) {
         self.0 |= BitBoard::from_sq(square).0;
     }
     // toggle bit at given square
-    pub fn toggle_bit(&mut self, square: Square) {
+    fn toggle_bit(&mut self, square: Square) {
         self.0 ^= BitBoard::from_sq(square).0;
+    }
+}
+
+impl Rotation for BitBoard {
+    fn rot_180(&mut self) {
+        let h1: BitBoard = BitBoard(0x5555555555555555);
+        let h2: BitBoard = BitBoard(0x3333333333333333);
+        let h4: BitBoard = BitBoard(0x0F0F0F0F0F0F0F0F);
+        let v1: BitBoard = BitBoard(0x00FF00FF00FF00FF);
+        let v2: BitBoard = BitBoard(0x0000FFFF0000FFFF);
+
+        self.0 = ((self.0 >> 1) & h1.0) | ((self.0 & h1.0) << 1);
+        self.0 = ((self.0 >> 2) & h2.0) | ((self.0 & h2.0) << 2);
+        self.0 = ((self.0 >> 4) & h4.0) | ((self.0 & h4.0) << 4);
+        self.0 = ((self.0 >> 8) & v1.0) | ((self.0 & v1.0) << 8);
+        self.0 = ((self.0 >> 16) & v2.0) | ((self.0 & v2.0) << 16);
+        self.0 = (self.0 >> 32) | (self.0 << 32);
     }
 }
 
@@ -142,5 +122,66 @@ impl Mul for BitBoard {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
         Self(self.0.wrapping_mul(rhs.0))
+    }
+}
+
+impl BitBoard {
+    // generate bitboard from square
+    pub fn from_sq(square: Square) -> Self {
+        Self(1 << square as u8)
+    }
+    // init empty bitboard
+    pub fn empty() -> Self {
+        Self(0)
+    }
+    // check if bitboard is empty (u64 = 0)
+    pub fn is_empty(&self) -> bool {
+        self.0 == 0
+    }
+    // check if equal
+    pub fn eq(&self, other: Self) -> bool {
+        self.0 == other.0
+    }
+    // generate bitboard from piece list
+    pub fn gen(pieces: &mut [Option<Piece>; 64], compare: char) -> Self {
+        let mut bin_str = String::new();
+        // reverse pieces array
+        pieces.reverse();
+
+        for piece in pieces {
+            if let Some(piece) = piece {
+                if !(piece.symbol == compare) {
+                    bin_str.push('0');
+                } else {
+                    bin_str.push('1');
+                }
+            } else {
+                bin_str.push('0');
+            }
+        }
+
+        // convert binary string to decimal (u64)
+        Self(u64::from_str_radix(&bin_str, 2).unwrap())
+    }
+    // print bitboard
+    fn print(&self) {
+        println!("Value: {}", &self.0);
+        let mut x = 8;
+        for rank in 0..8 {
+            print!("{}  ", x);
+            x -= 1;
+            for file in 0..8 {
+                let square = rank * 8 + file;
+                if self.0 & (1 << square) >= 1 {
+                    print!("1 ");
+                } else {
+                    print!("0 ");
+                }
+            }
+            println!();
+        }
+        println!();
+        println!("   a b c d e f g h");
+        println!();
     }
 }
