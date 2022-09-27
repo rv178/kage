9e828c04761c3a14f674f004c303afc1ddfb18a3
rv178 <idliyout@gmail.com>
2022-09-27 19:16:55 +0530

    [feat] knight attack generation

--- engine/src/bitboard.rs
+++ engine/src/bitboard.rs
@@ -1,5 +1,5 @@
-use crate::movegen::{bp_all_atk, wp_all_atk};
-use crate::{GameStatus, Piece, Square};
+use crate::movegen::{knight_atk_lookup, pawn_atk_lookup};
+use crate::{Colour, GameStatus, Piece, Square};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct BitBoard(pub u64);
@@ -42,10 +42,8 @@ impl BitPos {
 pub const UNIVERSAL: BitBoard = BitBoard(18446744073709551615);
 pub const NOT_A_FILE: BitBoard = BitBoard(18374403900871474942);
 pub const NOT_H_FILE: BitBoard = BitBoard(9187201950435737471);
-/*
-const NOT_HG_FILE: BitBoard = BitBoard(4557430888798830399);
-const NOT_AB_FILE: BitBoard = BitBoard(18229723555195321596);
-*/
+pub const NOT_HG_FILE: BitBoard = BitBoard(4557430888798830399);
+pub const NOT_AB_FILE: BitBoard = BitBoard(18229723555195321596);
 
 pub fn convert(game_status: &mut GameStatus) {
     let positions: BitPos = BitPos::new(&mut game_status.pieces);
@@ -54,79 +52,14 @@ pub fn convert(game_status: &mut GameStatus) {
     println!("Black pawns: ");
     positions.bp.print();
 
-    println!("Black pawn attacks:");
-    let bp_all = bp_all_atk(positions.bp);
-    bp_all.print();
-
     println!("White pawns:");
     positions.wp.print();
 
-    println!("White pawn attacks:");
-    let wp_all = wp_all_atk(positions.wp);
-    wp_all.print();
-}
+    let p = pawn_atk_lookup(Square::E4, Colour::Black);
+    p.print();
 
-// bit manipulation inside bitboard
-trait BitM {
-    fn set_bit(&mut self, square: Square);
-    fn toggle_bit(&mut self, square: Square);
-}
-
-// rotation methods
-trait Rotation {
-    fn rot_180(&mut self);
-}
-
-// flip methods
-trait Flip {
-    fn flip_v(&mut self);
-    fn flip_h(&mut self);
-}
-
-impl BitM for BitBoard {
-    // set bit at given square (0 -> 1)
-    fn set_bit(&mut self, square: Square) {
-        self.0 |= BitBoard::from_sq(square).0;
-    }
-    // toggle bit at given square
-    fn toggle_bit(&mut self, square: Square) {
-        self.0 ^= BitBoard::from_sq(square).0;
-    }
-}
-
-impl Rotation for BitBoard {
-    fn rot_180(&mut self) {
-        let h1: BitBoard = BitBoard(0x5555555555555555);
-        let h2: BitBoard = BitBoard(0x3333333333333333);
-        let h4: BitBoard = BitBoard(0x0F0F0F0F0F0F0F0F);
-        let v1: BitBoard = BitBoard(0x00FF00FF00FF00FF);
-        let v2: BitBoard = BitBoard(0x0000FFFF0000FFFF);
-
-        self.0 = ((self.0 >> 1) & h1.0) | ((self.0 & h1.0) << 1);
-        self.0 = ((self.0 >> 2) & h2.0) | ((self.0 & h2.0) << 2);
-        self.0 = ((self.0 >> 4) & h4.0) | ((self.0 & h4.0) << 4);
-        self.0 = ((self.0 >> 8) & v1.0) | ((self.0 & v1.0) << 8);
-        self.0 = ((self.0 >> 16) & v2.0) | ((self.0 & v2.0) << 16);
-        self.0 = (self.0 >> 32) | (self.0 << 32);
-    }
-}
-
-impl Flip for BitBoard {
-    fn flip_v(&mut self) {
-        let k1: BitBoard = BitBoard(0x00FF00FF00FF00FF);
-        let k2: BitBoard = BitBoard(0x0000FFFF0000FFFF);
-        self.0 = ((self.0 >> 8) & k1.0) | ((self.0 & k1.0) << 8);
-        self.0 = ((self.0 >> 16) & k2.0) | ((self.0 & k2.0) << 16);
-        self.0 = (self.0 >> 32) | (self.0 << 32);
-    }
-    fn flip_h(&mut self) {
-        let k1: BitBoard = BitBoard(0x5555555555555555);
-        let k2: BitBoard = BitBoard(0x3333333333333333);
-        let k4: BitBoard = BitBoard(0x0f0f0f0f0f0f0f0f);
-        self.0 = ((self.0 >> 1) & k1.0) | ((self.0 & k1.0) << 1);
-        self.0 = ((self.0 >> 2) & k2.0) | ((self.0 & k2.0) << 2);
-        self.0 = ((self.0 >> 4) & k4.0) | ((self.0 & k4.0) << 4);
-    }
+    let n = knight_atk_lookup(Square::E4);
+    n.print();
 }
 
 impl BitBoard {
@@ -145,6 +78,14 @@ impl BitBoard {
     // check if equal
     pub fn eq(&self, rhs: Self) -> bool {
         self.0 == rhs.0
+    }
+    // set bit at given square (0 -> 1)
+    pub fn set_bit(&mut self, square: Square) {
+        self.0 |= BitBoard::from_sq(square).0;
+    }
+    // toggle bit at given square
+    pub fn toggle_bit(&mut self, square: Square) {
+        self.0 ^= BitBoard::from_sq(square).0;
     }
     // generate bitboard from piece list
     pub fn gen(pieces: &mut [Option<Piece>; 64], compare: char) -> Self {
@@ -188,5 +129,38 @@ impl BitBoard {
         println!();
         println!("\x1b[34m   a b c d e f g h\x1b[0m");
         println!();
+    }
+
+    // rotate 180
+    pub fn rot_180(&mut self) {
+        let h1: BitBoard = BitBoard(0x5555555555555555);
+        let h2: BitBoard = BitBoard(0x3333333333333333);
+        let h4: BitBoard = BitBoard(0x0F0F0F0F0F0F0F0F);
+        let v1: BitBoard = BitBoard(0x00FF00FF00FF00FF);
+        let v2: BitBoard = BitBoard(0x0000FFFF0000FFFF);
+
+        self.0 = ((self.0 >> 1) & h1.0) | ((self.0 & h1.0) << 1);
+        self.0 = ((self.0 >> 2) & h2.0) | ((self.0 & h2.0) << 2);
+        self.0 = ((self.0 >> 4) & h4.0) | ((self.0 & h4.0) << 4);
+        self.0 = ((self.0 >> 8) & v1.0) | ((self.0 & v1.0) << 8);
+        self.0 = ((self.0 >> 16) & v2.0) | ((self.0 & v2.0) << 16);
+        self.0 = (self.0 >> 32) | (self.0 << 32);
+    }
+
+    // flip methods
+    pub fn flip_v(&mut self) {
+        let k1: BitBoard = BitBoard(0x00FF00FF00FF00FF);
+        let k2: BitBoard = BitBoard(0x0000FFFF0000FFFF);
+        self.0 = ((self.0 >> 8) & k1.0) | ((self.0 & k1.0) << 8);
+        self.0 = ((self.0 >> 16) & k2.0) | ((self.0 & k2.0) << 16);
+        self.0 = (self.0 >> 32) | (self.0 << 32);
+    }
+    pub fn flip_h(&mut self) {
+        let k1: BitBoard = BitBoard(0x5555555555555555);
+        let k2: BitBoard = BitBoard(0x3333333333333333);
+        let k4: BitBoard = BitBoard(0x0f0f0f0f0f0f0f0f);
+        self.0 = ((self.0 >> 1) & k1.0) | ((self.0 & k1.0) << 1);
+        self.0 = ((self.0 >> 2) & k2.0) | ((self.0 & k2.0) << 2);
+        self.0 = ((self.0 >> 4) & k4.0) | ((self.0 & k4.0) << 4);
     }
 }
--- engine/src/movegen.rs
+++ engine/src/movegen.rs
@@ -1,4 +1,9 @@
-use crate::bitboard::{BitBoard, NOT_A_FILE, NOT_H_FILE};
+use std::process::exit;
+
+use crate::bitboard::{BitBoard, NOT_AB_FILE, NOT_A_FILE, NOT_HG_FILE, NOT_H_FILE};
+use crate::{Colour, Square};
+
+// pawn attacks
 
 pub fn bp_east_atk(board: BitBoard) -> BitBoard {
     BitBoard((board.0 << 9) & NOT_A_FILE.0)
@@ -26,4 +31,72 @@ pub fn wp_all_atk(board: BitBoard) -> BitBoard {
     let east = wp_east_atk(board);
     let west = wp_west_atk(board);
     BitBoard(east.0 | west.0)
+}
+
+pub fn pawn_atk_lookup(square: Square, colour: Colour) -> BitBoard {
+    let board = BitBoard::from_sq(square);
+
+    match colour {
+        Colour::White => wp_all_atk(board),
+        Colour::Black => bp_all_atk(board),
+        Colour::Undefined => exit(1),
+    }
+}
+
+// knight attacks
+
+pub fn north_east_knight_atk(board: BitBoard) -> BitBoard {
+    BitBoard((board.0 << 17) & NOT_A_FILE.0)
+}
+
+pub fn north_west_knight_atk(board: BitBoard) -> BitBoard {
+    BitBoard((board.0 << 15) & NOT_H_FILE.0)
+}
+
+pub fn south_east_knight_atk(board: BitBoard) -> BitBoard {
+    BitBoard((board.0 >> 15) & NOT_A_FILE.0)
+}
+
+pub fn south_west_knight_atk(board: BitBoard) -> BitBoard {
+    BitBoard((board.0 >> 17) & NOT_H_FILE.0)
+}
+
+pub fn east_north_knight_atk(board: BitBoard) -> BitBoard {
+    BitBoard((board.0 << 10) & NOT_AB_FILE.0)
+}
+
+pub fn east_south_knight_atk(board: BitBoard) -> BitBoard {
+    BitBoard((board.0 >> 6) & NOT_AB_FILE.0)
+}
+
+pub fn west_north_knight_atk(board: BitBoard) -> BitBoard {
+    BitBoard((board.0 << 6) & NOT_HG_FILE.0)
+}
+
+pub fn west_south_knight_atk(board: BitBoard) -> BitBoard {
+    BitBoard((board.0 >> 10) & NOT_HG_FILE.0)
+}
+
+pub fn knight_atk_lookup(square: Square) -> BitBoard {
+    let board = BitBoard::from_sq(square);
+
+    let north_east = north_east_knight_atk(board);
+    let north_west = north_west_knight_atk(board);
+    let south_east = south_east_knight_atk(board);
+    let south_west = south_west_knight_atk(board);
+    let east_north = east_north_knight_atk(board);
+    let east_south = east_south_knight_atk(board);
+    let west_north = west_north_knight_atk(board);
+    let west_south = west_south_knight_atk(board);
+
+    BitBoard(
+        north_east.0
+            | north_west.0
+            | south_east.0
+            | south_west.0
+            | east_north.0
+            | east_south.0
+            | west_north.0
+            | west_south.0,
+    )
 }
